6f40ddcc3916afa385fcbff1e71db47d4f6b6c1e
leozero3 <realleo2009@hotmail.com>
2020-12-29 17:42:44 -0300

    modal

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
 
-
 class TransactionForm extends StatelessWidget {
-
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
   final void Function(String, double) onSubmit;
 
-
   TransactionForm(this.onSubmit);
 
-    @override
-    Widget build(BuildContext context) {
-      return Card(
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  labelText: 'Titulo',
-                ),
-              ),
-              TextField(
-                controller: valueController,
-                decoration: InputDecoration(
-                  labelText: 'Valor(R\$)',
-                ),
-              ),
-              FlatButton(
-                onPressed: () {
-                  final title = titleController.text;
-                  final value = double.tryParse(valueController.text) ?? 0.0;
-                  onSubmit(title, value);
-                },
-                child: Text('Nova Transação'),
-                textColor: Colors.purple,
-              )
-            ],
-          ),
-        ),
-      );
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
     }
+    onSubmit(title, value);
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Titulo',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Valor(R\$)',
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                _submitForm();
+              },
+              child: Text('Nova Transação'),
+              textColor: Colors.purple,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'dart:math';
 import 'package:despesas/components/transaction_form.dart';
 import 'package:despesas/components/transaction_list.dart';
-import 'package:despesas/components/transaction_user.dart';
 import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -12,16 +11,62 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'novo tenis',
+      value: 315.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Luz',
+      value: 423.55,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormMoldal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(null);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _openTransactionFormMoldal(context)),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -33,10 +78,15 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
-            TransactionUser()
+            TransactionList(_transactions),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormMoldal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
